23aa543afb02f5be82b816f8bb6fdc3631126a9f
Miraclev1 <fathiralmsyah3@gmail.com>
2024-06-26 18:53:07 +0700

    X6831: drop vendor logtag

--- device.mk
+++ device.mk
@@ -370,9 +370,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     PowerOffAlarm
 
-# Properties
--include $(LOCAL_PATH)/vendor_logtag.mk
-
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0 \
